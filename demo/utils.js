f3c8aa5f6889f741ac6ea09337e3156db6feb1d1
Kiwi Liu <kiwi71728@icloud.com>
2017-11-30 17:41:19 +0800

    Updates

--- demo/utils.js
+++ demo/utils.js
@@ -75,7 +75,7 @@ function getConfig() {
       state.distScriptSrc = `../../../dist/${state.version}/player-sdk.js`
       break
     case 'staging':
-      state.distScriptSrc = 'https://app.straas.net/sdk/1.8.0/player-sdk.js'
+      state.distScriptSrc = 'https://app.straas.net/sdk/1.9.0/player-sdk.js'
       break
     default:
       logMessage(`
@@ -84,7 +84,7 @@ function getConfig() {
         若沒有選取環境，默認為 production
       `)
     case 'production':
-      state.distScriptSrc = 'https://app.straas.net/sdk/1.8.0/player-sdk.js'
+      state.distScriptSrc = 'https://app.straas.net/sdk/1.9.0/player-sdk.js'
       break
   }
 
